0143274748296b39163d2fd0b7d9181141726d49
JoyVoid <88936584+JoyVoid@users.noreply.github.com>
2022-01-18 01:59:20 -0800

    Add files via upload

--- data/misc/django_init.sql
+++ data/misc/django_init.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS nc_tutorials_db;
-CREATE DATABASE nc_tutorials_db;
+DROP DATABASE IF EXISTS nctutorials2;
+CREATE DATABASE nctutorials2;
 
-\c nc_tutorials_db
+\c nctutorials2
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
